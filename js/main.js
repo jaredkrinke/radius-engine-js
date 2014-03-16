f568bf8107804f7f75a53550a591460dffc18c52
jaredkrinke <jaredkrinke@gmail.com>
2014-03-16 13:53:13 -0700

    Initial template

---
+++ js/main.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="radius.js" />
+/// <reference path="radius-ui.js" />
+
+function GameLayer() {
+    Layer.call(this);
+}
+
+GameLayer.prototype = Object.create(Layer.prototype);
+
+window.addEventListener('DOMContentLoaded', function () {
+    Radius.initialize(document.getElementById('canvas'));
+    Radius.start(new GameLayer());
+});
